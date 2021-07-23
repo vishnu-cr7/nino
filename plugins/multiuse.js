e2c4566383d1cbf8979bd6183a7ac9114a7fdd39
Tony-alex1 <87007820+Tony-alex1@users.noreply.github.com>
2021-07-23 19:34:25 +0530

    Update multiuse.js

--- plugins/multiuse.js
+++ plugins/multiuse.js
@@ -7,8 +7,8 @@ Asena.addCommand({ pattern: 'insta ?(.*)', fromMe: false, desc: IG_DESC }, async
     //if(match[1] == '') return
     let { data, type } = await instaGram(match[1], 'f3eaf19231f6201c');
     //if(type == undefined) return 
-    if (type === 'image') { await message.sendMessage(data, MessageType.image, { caption: "Made By JulieMwol" }) }
-    else if (type === 'video') { await message.sendMessage(data, MessageType.video, { caption: "Made By JulieMwol" }) }
+    if (type === 'image') { await message.sendMessage(data, MessageType.image, { caption: "Made by marvel bot📲" }) }
+    else if (type === 'video') { await message.sendMessage(data, MessageType.video, { caption: "Made by Marvel bot 👩‍💻" }) }
 });
 //const axios = require('axios')
 async function instaGram(url, key){
